008cffae9d0ee2d10b0dbece02f315c243f54ac5
Vayentha <sayanpsur@gmail.com>
2021-01-18 22:32:15 -0700

    add possible fix ??

--- rundaemon.py
+++ rundaemon.py
@@ -9,6 +9,7 @@ class MyDaemon(dd.NSDaemon):
 
 
 
+
 if __name__ == "__main__":
     daemon = MyDaemon('/tmp/daemon-example.pid')
     if len(sys.argv) == 2:
@@ -19,11 +20,9 @@ if __name__ == "__main__":
         elif 'restart' == sys.argv[1]:
             daemon.restart()
         else:
-            print
-            "Unknown command"
+            print("Unknown command")
             sys.exit(2)
         sys.exit(0)
     else:
-        print
-        "usage: %s start|stop|restart" % sys.argv[0]
+        print("usage: %s start|stop|restart" % sys.argv[0])
         sys.exit(2)
